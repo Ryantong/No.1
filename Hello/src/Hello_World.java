2fc1fa33281d01b4b2e5776d5ca01169844170d1
Ryantong <20tongr31@mymhs.org>
2018-08-20 09:45:43 -0700

    First Project to commit in apcs 2018

---
+++ Hello/src/Hello_World.java
@@ -0,0 +1,13 @@
+
+public class Hello_World {
+	/**
+	 * Print out a message saying Hello!
+	 * 
+	 * @author Ryan Tong
+	 * @version 8/17/2018
+	 */
+	
+	public static void main(String[]args){
+		System.out.println("Hello World! From Ryan");
+	}
+}
