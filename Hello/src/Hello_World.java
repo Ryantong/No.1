1846074f4a7a7a99555d2941c5c429d5d25946fb
Ryantong <20tongr31@mymhs.org>
2018-08-29 10:16:12 -0700

    comment above Class header

--- Hello/src/Hello_World.java
+++ Hello/src/Hello_World.java
@@ -1,12 +1,11 @@
 
+/**
+ * Print out a message saying Hello!
+ * 
+ * @author Ryan Tong
+ * @version 8/17/2018
+ */
 public class Hello_World {
-	/**
-	 * Print out a message saying Hello!
-	 * 
-	 * @author Ryan Tong
-	 * @version 8/17/2018
-	 */
-	
 	public static void main(String[]args){
 		System.out.println("Hello World! From Ryan");//Display the string
 	}
