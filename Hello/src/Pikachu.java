7971f76e8ded659fc497890326cf6df2d1d59963
Ryantong <20tongr31@mymhs.org>
2018-08-31 09:51:50 -0700

    Pikachu

--- Hello/src/Pikachu.java
+++ Hello/src/Pikachu.java
@@ -1,4 +1,11 @@
-
+/*Print this:
+ *  Pikachu welcomes you to the world of Pokemon!
+                (\__/)
+                (o^.^)
+              z(_(")(")
+    Ryan Tong
+    
+ */
 public class Pikachu {
 	public static void main(String[] args){
 		System.out.println("Pikachu welcomes you to the world of Pokemon!");
